46eab7f34fa3daa76e7b8f44b2d71de19642aa2c
Mknauss <mknauss97@gmail.com>
2021-07-22 18:56:15 -0400

    things now show

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+const header = () => {
+
+    return (
+        <div className="text-center">
+            <h1>Employee Directory</h1>
+           
+        </div>
+
+
+    )
+}
+
+export default header;
